241c0353d1801ff1a6d3e6f8b2844f692a045cff
Piotr Łojko <80154448+S19514@users.noreply.github.com>
2022-02-20 20:28:35 +0100

    Initial Commit With completed task

---
+++ Controllers/DetailsController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.IO;
+using Zadanie2.Models;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Zadanie2.Controllers
+{
+    public class DetailsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Create()
+        {                      
+            return View();
+        }
+        //  var Metoda = Request.Method;
+        //var czas = (DateTime)HttpContext.Items["RequestTimestamp"];
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Details details)
+        {
+            if (ModelState.IsValid)
+            {
+                details.dateTime = (DateTime)HttpContext.Items["RequestTimestamp"];
+                details.RequestType = Request.Method;
+                details.ClientIPAdress = Request.Host.Host;
+
+                using (StreamWriter writer = System.IO.File.AppendText(@"C:\Users\piotr\Desktop\onninen\output\Data.txt"))
+                {
+                    writer.WriteLine($"{JsonSerializer.Serialize(details)}");
+                }
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Models/Details.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+namespace Zadanie2.Models
+{
+    public class Details
+    {
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Pole nie może być puste")]
+        [StringLength(10, MinimumLength = 10, ErrorMessage = "NIP powinien zawierac 10 znaków.")]
+        public string NIP { get; set; }
+        public string RequestType { get; set; }
+        public string ClientIPAdress { get; set; }
+        public DateTime dateTime { get; set; }
+    }
+}
